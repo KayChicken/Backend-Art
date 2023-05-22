81e83b39bdc5693ac1f392cb5eee978dc4204876
KayChicken <kainov.dmitry370@yandex.ru>
2023-05-22 11:24:24 +0300

    auth

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
 export const tasksRouter = new Router()
 import { tasksController } from '../controller/tasks.controller.js'
+import checkAuth from '../utils/checkAuth.js'
 
 
 
 
-tasksRouter.get('/tasks/quiz',tasksController.getQuizes)
-tasksRouter.get('/tasks/quiz/:id',tasksController.getOneQuiz)
+tasksRouter.get('/tasks/quiz',checkAuth,tasksController.getQuizes)
+tasksRouter.get('/tasks/quiz/:id',checkAuth,tasksController.getOneQuiz)
