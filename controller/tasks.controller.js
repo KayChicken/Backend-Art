e37dc3595cab8679727af107e571c38bc1e059f3
KayChicken <kainov.dmitry370@yandex.ru>
2023-05-22 14:30:02 +0300

    quiz fix

--- controller/tasks.controller.js
+++ controller/tasks.controller.js
@@ -29,6 +29,20 @@ class TasksController {
         }
     }
 
+
+    async answerByUser(req,res) {
+        try {
+            const {quiz_id, user_answer} = req.body
+            const id = req.userId
+            const sendAnswer = await db.query("INSERT INTO tasks_quiz_answers VALUES ($1,$2,$3)",[quiz_id,id,user_answer])
+            res.status(200).json(sendAnswer.rows)
+        }
+
+        catch(err) {
+            return res.status(400).json({"message" : "Произошла ошибка"})
+        }
+    }
+
 }
 
 
