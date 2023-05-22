f5e7b59f2a764aae6fff18dbb28c2c74572e7edb
KayChicken <kainov.dmitry370@yandex.ru>
2023-05-22 19:14:20 +0300

    add uploads

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.json())
 
-
+app.use('/uploads', express.static('uploads'))
 app.use(newsRouter)
 app.use(userRouter)
 app.use(tasksRouter)
