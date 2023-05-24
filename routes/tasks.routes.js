2611e69ace242c8608bc8ca57b240515ed49a94f
KayChicken <kainov.dmitry370@yandex.ru>
2023-05-24 13:51:07 +0300

    add API

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -9,3 +9,4 @@ import checkAuth from '../utils/checkAuth.js'
 tasksRouter.get('/tasks/quiz',checkAuth,tasksController.getQuizes)
 tasksRouter.get('/tasks/quiz/:id',checkAuth,tasksController.getOneQuiz)
 tasksRouter.post('/tasks/quiz' , checkAuth, tasksController.answerByUser)
+tasksRouter.get('/tasks/videos', checkAuth , tasksController.getVideoGame)
