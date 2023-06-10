9636d744d15ea316317b31301619e3c332726256
KayChicken <105989236+KayChicken@users.noreply.github.com>
2023-06-10 17:34:43 +0300

    Update tasks.controller.js

--- controller/tasks.controller.js
+++ controller/tasks.controller.js
@@ -107,10 +107,10 @@ class TasksController {
     
     async createTask(req, res) {
 	    try {
-		    const {task_question, task_answers, task_difficult, task_correct_answer, task_correct_desc} = req.body;
+		const {task_question, task_answers, task_difficult, task_correct_answer, task_correct_desc} = req.body;
     		const newTask = await db.query(`INSERT INTO task_quiz (task_question, task_answers, task_difficult, task_correct_answer, task_correct_desc) VALUES ${task_question} ${task_answers} ${task_difficult} ${task_correct_answer} ${task_correct_desc}`);
 	    	return res.status(200).json(newTask.rows);
-    	} catch (err0) {
+    	} catch (err) {
     		return res.status(400).json({"message": "Произошла ошибка"});
 	    }
     }
