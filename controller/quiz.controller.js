e4d44135cf1bcb95160855d70f8bb428494b3799
sashasevostyanov <sevostanovsasa7@gmail.com>
2023-06-10 19:51:33 +0300

    add some

--- controller/quiz.controller.js
+++ controller/quiz.controller.js
@@ -32,4 +32,4 @@ class QuizController {
   }
 }
 
-module.exports = new QuizController();
+export const quizController = new QuizController
--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express'
+import {quizController} from "../controller/quiz.controller.js";
 export const quizRoutes = new Router()
-const quizController = require('../controller/quiz.controller.js')
 
 quizRoutes.post('quiz/create', quizController.createQuiz);
 quizRoutes.post('/quiz/answer/add', quizController.addAnswer);
